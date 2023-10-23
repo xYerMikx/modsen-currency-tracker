8d8ddbe6d9d280285670c09211583b67daf3eba8
Yan Yermalayeu <yermik2014@yandex.ru>
2023-10-23 11:46:06 +0300

    init: init project with configuration

---
+++ src/constants/theme.js
@@ -0,0 +1,19 @@
+const theme = {
+	gradientColor: "linear-gradient(90deg, #00CE2C 0.18%, #AEDF23 49.3%, #A3DC00 99.88%)",
+	black: "#000000",
+	orange: "#FF971D",
+	dark: {
+		primaryColor: "#ffffff",
+		secondaryColor: "#d9d9d9",
+		darkColor: "#898989",
+		lightGray: "#A7B2C3",
+		bgColor: "#030304",
+	},
+	light: {
+		primaryColor: "#000000",
+		secondaryColor: "#010101",
+		darkColor: "#898989",
+		bgColor: "#ffffff",
+	},
+}
+export default theme
