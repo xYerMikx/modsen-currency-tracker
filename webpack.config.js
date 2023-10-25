e0d96a6840cd88e53a6c8ab2ad394e8360742c0e
Yan Yermalayeu <yermik2014@yandex.ru>
2023-10-25 23:34:22 +0300

    fix: fix webpack config using webpack-merge

--- webpack.config.js
+++ webpack.config.js
@@ -3,63 +3,43 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 const { HotModuleReplacementPlugin } = require("webpack")
 const ESLintWebpackPlugin = require("eslint-webpack-plugin")
-const TerserPlugin = require("terser-webpack-plugin")
 
-module.exports = (_, argv) => {
-	const isProd = argv.mode === "production"
-
-	return {
-		entry: ["@babel/polyfill", path.join(__dirname, "src", "index.jsx")],
-		output: {
-			path: path.resolve(__dirname, "dist"),
-			filename: "bundle.js",
-		},
-		devServer: isProd
-			? {}
-			: {
-					host: "localhost",
-					port: 3000,
-					open: true,
-					hot: true,
-			  },
-		module: {
-			rules: [
-				{
-					test: /\.(js|jsx)$/,
-					exclude: /node_modules/,
-					use: {
-						loader: "babel-loader",
-					},
+module.exports = {
+	entry: ["@babel/polyfill", path.join(__dirname, "src", "index.jsx")],
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
 				},
-				{
-					test: /\.(css|scss)$/,
-					use: ["style-loader", "css-loader", "sass-loader"],
-				},
-				{
-					test: /\.(jpg|jpeg|png|svg)$/,
-					use: ["file-loader"],
-				},
-			],
-		},
-		plugins: [
-			new HtmlWebpackPlugin({
-				template: path.join(__dirname, "public", "index.html"),
-				title: "Modsen Currency Tracker",
-				favicon: path.join(__dirname, "public", "favicon.svg"),
-			}),
-			new CleanWebpackPlugin(),
-			new HotModuleReplacementPlugin(),
-			new ESLintWebpackPlugin(),
+			},
+			{
+				test: /\.(css|scss)$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.(jpg|jpeg|png|svg)$/,
+				use: ["file-loader"],
+			},
 		],
-		resolve: {
-			extensions: [".jsx", ".js", ".scss"],
-		},
-		devtool: isProd ? false : "source-map",
-		optimization: isProd
-			? {
-					minimize: true,
-					minimizer: [new TerserPlugin()],
-			  }
-			: {},
-	}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, "public", "index.html"),
+			title: "Modsen Currency Tracker",
+			favicon: path.join(__dirname, "public", "favicon.svg"),
+		}),
+		new CleanWebpackPlugin(),
+		new HotModuleReplacementPlugin(),
+		new ESLintWebpackPlugin(),
+	],
+	resolve: {
+		extensions: [".jsx", ".js", ".scss"],
+	},
 }
