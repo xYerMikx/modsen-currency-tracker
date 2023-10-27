d0bb826c27e00205ade3b533fa740863a3a35551
Yan Yermalayeu <yermik2014@yandex.ru>
2023-10-27 23:05:51 +0300

    feat: add dropdown

---
+++ src/components/Dropdown/Dropdown.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+import {
+	ArrowIcon,
+	DropDownContainer,
+	DropDownHeader,
+	DropDownList,
+	DropDownListContainer,
+	ListItem,
+	Main,
+} from "./styled"
+import arrow from "@assets/icons/arrow-down.svg"
+
+const options = ["USD", "EUR", "CAD", "JPY"]
+
+const Dropdown = ({ selectedOption, setSelectedOption }) => {
+	const [isOpen, setIsOpen] = useState(false)
+
+	const toggling = () => setIsOpen(!isOpen)
+
+	const onOptionClicked = (value) => () => {
+		setSelectedOption(value)
+		setIsOpen(false)
+	}
+
+	return (
+		<Main>
+			<DropDownContainer>
+				<DropDownHeader onClick={toggling}>
+					<p>{selectedOption || "USD"}</p>
+					<ArrowIcon src={arrow} />
+				</DropDownHeader>
+				{isOpen && (
+					<DropDownListContainer>
+						<DropDownList>
+							{options.map((option, index) => (
+								<ListItem onClick={onOptionClicked(option)} key={index}>
+									{option}
+								</ListItem>
+							))}
+						</DropDownList>
+					</DropDownListContainer>
+				)}
+			</DropDownContainer>
+		</Main>
+	)
+}
+
+export default Dropdown
