cbd28656cf80d5a8b01b7336c4a702bd0ea23312
Yan Yermalayeu <yermik2014@yandex.ru>
2023-11-07 18:59:25 +0300

    feat: add cards responsiveness

--- src/components/Card/styled.js
+++ src/components/Card/styled.js
@@ -1,5 +1,7 @@
 import styled from "styled-components"
 
+import { media } from "@/constants/sizes"
+
 export const CardWrapper = styled.div`
   display: flex;
   background-color: ${(props) => props.theme.cardBgColor};
@@ -8,8 +10,17 @@ export const CardWrapper = styled.div`
   border: 0.1rem solid ${(props) => props.theme.borderColor};
   gap: 3rem;
   font-size: ${(props) => props.theme.fontSize.sm};
-  flex: 0 1 40%;
+  flex: 0 1 47.5%;
   cursor: pointer;
+
+  @media ${media.screen} {
+    gap: 1.5rem;
+  }
+
+  @media ${media.phone} {
+    flex: 1 0 100%;
+    width: 100%;
+  }
 `
 
 export const CardTitle = styled.h3`
@@ -19,9 +30,22 @@ export const CardTitle = styled.h3`
 export const CardText = styled.p`
   color: ${(props) => props.theme.lightGray};
   font-size: ${(props) => props.theme.fontSize.md};
+
+  @media ${media.tablet} {
+    font-size: ${(props) => props.theme.fontSize.sm};
+  }
 `
 
 export const Image = styled.img`
   max-width: 8rem;
   max-height: 8rem;
+
+  @media ${media.screen} {
+    max-width: 6rem;
+    max-height: 6rem;
+  }
+  @media ${media.tablet} {
+    max-width: 5rem;
+    max-height: 5rem;
+  }
 `
--- src/components/StocksList/styled.js
+++ src/components/StocksList/styled.js
@@ -1,9 +1,22 @@
 import styled from "styled-components"
 
+import { media } from "@/constants/sizes"
+
 export const StocksContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
   row-gap: 5rem;
   margin-bottom: ${(props) => props.theme.spacings.xxmd};
+
+  @media ${media.tablet} {
+    justify-content: space-around;
+    column-gap: 1rem;
+  }
+
+  @media ${media.phone} {
+    flex-direction: column;
+    align-items: center;
+    gap: 2.5rem;
+  }
 `
