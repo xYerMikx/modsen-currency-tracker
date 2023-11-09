8268457f71ee346cfdadcccc8b1af020c1645560
Yan Yermalayeu <yermik2014@yandex.ru>
2023-11-09 09:45:06 +0300

    feat: add chart e2e tests

---
+++ cypress/e2e/chart.cy.js
@@ -0,0 +1,34 @@
+describe("Timeline page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/timeline") // замените на URL вашей страницы Timeline
+  })
+
+  it("should display notification when visited /timeline", () => {
+    cy.get('[data-cy="notification"]').should(
+      "contain",
+      "График за 30 дней был успешно построен",
+    )
+    cy.get('[data-cy="notification"]').should("be.visible")
+  })
+
+  it('should display notification on "Update Chart" click', () => {
+    cy.get("button").contains("Update Chart").click()
+    cy.get('[data-cy="notification"]').should(
+      "contain",
+      "График за 30 дней был успешно построен",
+    )
+    cy.get('[data-cy="notification"]').should("be.visible")
+  })
+  it('should update chart data on "Randomize values" click', () => {
+    cy.get("canvas").should("exist")
+
+    cy.get("button").contains("Randomize values").click()
+    cy.get("button").contains("Update Chart").click()
+
+    cy.get('[data-cy="notification"]').should(
+      "contain",
+      "График за 30 дней был успешно построен",
+    )
+    cy.get('[data-cy="notification"]').should("be.visible")
+  })
+})
